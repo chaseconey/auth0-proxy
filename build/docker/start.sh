45cadea66d7927ce08d1a51b6f9f7a06fc27bdf0
Chase Coney <chasiepoo@gmail.com>
2022-02-23 16:06:12 -0600

    feat: add sqlite support

--- build/docker/start.sh
+++ build/docker/start.sh
@@ -4,11 +4,13 @@
 ##
 
 # Cache in Laravel
-cd /var/www/php
 php artisan config:cache
 
 # Run Migrations
 php artisan migrate --force
+
+# Make sqlite db available if chosen
+touch database/database.sqlite
 
 # Execute original docker start script
 apache2-foreground
