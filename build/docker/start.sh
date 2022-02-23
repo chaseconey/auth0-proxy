b6679deeafe6de48c7d1c8be43227bd2a1c2320b
Chase Coney <chasiepoo@gmail.com>
2022-02-23 17:03:26 -0600

    fix: config caching

--- build/docker/start.sh
+++ build/docker/start.sh
@@ -3,6 +3,10 @@
 ## Script run by on container start -- see Dockerfile
 ##
 
+# Ensure fresh caches
+php artisan config:clear
+php artisan config:cache
+
 # Make sqlite db available if chosen
 touch database/database.sqlite
 
