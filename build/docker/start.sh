bfed5ab817ce7af0070e1daa5cdf56b4afee39c8
Chase Coney <chasiepoo@gmail.com>
2022-02-23 16:09:22 -0600

    fix: sqlite docker instantiation

--- build/docker/start.sh
+++ build/docker/start.sh
@@ -6,11 +6,11 @@
 # Cache in Laravel
 php artisan config:cache
 
-# Run Migrations
-php artisan migrate --force
-
 # Make sqlite db available if chosen
 touch database/database.sqlite
+
+# Run Migrations
+php artisan migrate --force
 
 # Execute original docker start script
 apache2-foreground
